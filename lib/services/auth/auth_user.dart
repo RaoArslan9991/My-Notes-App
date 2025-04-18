1dacde2c369c99d0e9b8522b78495ef4a15d6de8
RaoArslan9991 <arslanaslam2004.com>
2025-04-19 00:50:25 +0500

    step-27

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -2,20 +2,20 @@ import 'package:firebase_auth/firebase_auth.dart' show User;
 import 'package:flutter/cupertino.dart';
 
 @immutable
-class AuthUser{
+@immutable
+class AuthUser {
   final String id;
   final String email;
   final bool isEmailVerified;
   const AuthUser({
     required this.id,
-    required this.email, 
-    required this.isEmailVerified
+    required this.email,
+    required this.isEmailVerified,
   });
 
-  factory AuthUser.formFirebase(User user) => 
-    AuthUser(
-      id: user.uid,
-      email: user.email!,
-      isEmailVerified:  user.emailVerified,  
-    );
+  factory AuthUser.fromFirebase(User user) => AuthUser(
+        id: user.uid,
+        email: user.email!,
+        isEmailVerified: user.emailVerified,
+      );
 }
---
+++ lib/utilities/dialogs/password_reset_email_send_dialog.dart
@@ -0,0 +1,14 @@
+import 'package:first_app/utilities/dialogs/generic_dialog.dart';
+import 'package:flutter/material.dart';
+
+Future<void> showPasswordResetSentDialog(BuildContext context) {
+  return showGenericDialog<void>(
+    context: context,
+    title: 'Password Reset',
+    content:
+        'We have now sent you a password reset link. Please check your email for more information.',
+    optionsBuilder: () => {
+      'OK': null,
+    },
+  );
+}
